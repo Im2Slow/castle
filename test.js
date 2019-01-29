80bc7a742090105479da023e960e6136fdcafbd5
Im2Slow <adrien.kerisit@gmail.com>
2019-01-29 04:39:16 +0100

    Tried select inner data from script tags with xpath.. Not conclusive yet

--- test.js
+++ test.js
@@ -2,13 +2,12 @@ let fs = require('fs');                                                         
 let axios = require('axios');                                                   // axios sert à réaliser une requête http
 let xpath = require('xpath');
 let DOMParser = require('xmldom').DOMParser;
-
+var xpathresult = "";
 axios('https://www.relaischateaux.com/fr/destinations/europe/france').then((response) => {  // on demande ici à axios d'aller sur relaischateaux.com, si la réponse est bonne...
-fs.writeFileSync('hotel.html', response.data);                                 // alors fs enregistre le code html dans le fichier hotel.html
+xpathresult = xpath.select("//script[@type='text/javascript']/text()[last()]",response.data);                                // alors fs enregistre le code html dans le fichier hotel.html
 });
-var content = String(fs.readFileSync("hotel.html"));
+//var content = String(fs.readFileSync("hotel.html"));
 // var raw_data = String(content).slice(678,1429);
 // fs.writeFileSync('hotel2.json', raw_data);
-var doc = new DOMParser().parseFromString(content, 'text/html');
-var xpathresult = xpath.select("//script[contains(.,'oMapOptionsdestinationfranceMap')]",doc);
+//var doc = new DOMParser().parseFromString(content, 'text/html');
 console.log(xpathresult);
